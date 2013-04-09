1528ac5bec3dacbc1b8161353048e644d786273e
Håkon Drolsum Røkenes <hadr@steria.no>
2013-04-09 21:09:46 +0200

    added node/edge classes + support for pos/deprel weights

--- algorithm/edge_edit_dist.py
+++ algorithm/edge_edit_dist.py
@@ -3,33 +3,33 @@ import sys
 
 
 class EdgeEditDistance(AbstractGraphEditDistance):
-	"""
-	Calculates the graph edit distance between two edges.
-	A node in this context is interpreted as a graph,
-	and edges are interpreted as nodes.
-	"""
+    """
+    Calculates the graph edit distance between two edges.
+    A node in this context is interpreted as a graph,
+    and edges are interpreted as nodes.
+    """
 
-	def insert_cost(self, i, j):
-		if i == j:
-			return deprel_insdel_weight(self.g2.nodes[j])
-		return sys.maxint
+    def __init__(self, g1, g2, deprel_weights):
+        AbstractGraphEditDistance.__init__(g1, g2)
+        self.deprel_weights = deprel_weights
 
+    def insert_cost(self, i, j):
+        if i == j:
+            return self.deprel_insdel_weight(self.g2.nodes[j])
+        return sys.maxint
 
-	def delete_cost(self, i, j):
-		if i == j:
-			return deprel_insdel_weight(self.g1.nodes[i])
-		return sys.maxint
+    def delete_cost(self, i, j):
+        if i == j:
+            return self.deprel_insdel_weight(self.g1.nodes[i])
+        return sys.maxint
 
-   	def substitute_cost(self, edge1, edge2):
-   		if edge1 == edge2:
-   			return 0.
-   		return deprel_sub_weight(edge1, edge2) 
+    def substitute_cost(self, edge1, edge2):
+        if edge1 == edge2:
+            return 0.
+        return self.deprel_sub_weight(edge1, edge2)
 
+    def deprel_sub_weight(self, edge1, edge2):
+        return self.deprel_weights(edge1.deprel+"-"+edge2.deprel)
 
-def deprel_sub_weight(edge1, edge2):
-    #TODO: fetch weights from file 
-    return 1.
-
-def deprel_insdel_weight(edge):
-    #TODO: fetch weights from file 
-    return 1.
+    def deprel_insdel_weight(self, edge):
+        return self.deprel_weights(edge.deprel)
--- classes/models.py
+++ classes/models.py
@@ -1,8 +1,10 @@
 # -*- coding: UTF-8 -*-
 from utils.misc import list_diff
 
+
 class Node():
-    def __init__(self, id_, attributes):
+
+    def __init__(self, id_, **attributes):
         self.id_ = id_
         self.attributes = attributes
 
@@ -13,15 +15,15 @@ class Node():
         return hash(self.__key())
 
     def __repr__(self):
-        return self.id_
-    
+        return self.attributes['word']
+
     def __eq__(self, other):
-        if other == None:
+        if other is None:
             return False
         return self.id_ == other.id_
 
     def eq_attrs(self, other):
-        if other == None:
+        if other is None:
             return False
         return self.attributes == other.attributes
 
@@ -31,41 +33,20 @@ class Node():
         if diff == 0:
             return 0.
         return len(self.attributes) / float(diff)
-        
 
 
 class Edge():
-    def __init__(self, start, end, attributes):
+
+    def __init__(self, start, end, **attributes):
         self.start = start
         self.end = end
         self.attributes = attributes
 
-
-class Attribute():
-    def __init__(self, name, var, vartype):
-        self.name = name
-        if vartype == 'float':
-            self.var = float(var)
-        elif vartype == 'int':
-            self.var = int(var)
-        else:
-            self.var = var
-        self.vartype = vartype
-
     def __repr__(self):
-        return self.name + ":" + str(self.var) + " " + self.vartype
-
-    def __key(self):
-        return self.name, self.var, self.vartype
+        return self.attributes['deprel']
 
     def __hash__(self):
-        return hash(self.__key())
+        return hash(self.attributes)
 
     def __eq__(self, other):
-        if other == None:
-            return False
-        if (self.name == other.name and
-            self.var == other.var and
-            self.vartype == other.vartype):
-            return True
-        return False
+        return self.attributes == other.attributes
