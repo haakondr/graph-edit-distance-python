94f14cbb223b4166b6efc43f30e9eb5630621b9f
Håkon Drolsum Røkenes <hadr@steria.no>
2013-04-09 21:09:46 +0200

    added node/edge classes + support for pos/deprel weights

--- algorithm/edge_edit_dist.py
+++ algorithm/edge_edit_dist.py
@@ -9,27 +9,29 @@ class EdgeEditDistance(AbstractGraphEditDistance):
 	and edges are interpreted as nodes.
 	"""
 
+	def __init__(self, g1, g2, deprel_weights):
+		AbstractGraphEditDistance.__init__(g1, g2)
+		self.deprel_weights = deprel_weights
+
 	def insert_cost(self, i, j):
 		if i == j:
-			return deprel_insdel_weight(self.g2.nodes[j])
+			return self.deprel_insdel_weight(self.g2.nodes[j])
 		return sys.maxint
 
 
 	def delete_cost(self, i, j):
 		if i == j:
-			return deprel_insdel_weight(self.g1.nodes[i])
+			return self.deprel_insdel_weight(self.g1.nodes[i])
 		return sys.maxint
 
    	def substitute_cost(self, edge1, edge2):
    		if edge1 == edge2:
    			return 0.
-   		return deprel_sub_weight(edge1, edge2) 
+   		return self.deprel_sub_weight(edge1, edge2) 
 
 
-def deprel_sub_weight(edge1, edge2):
-    #TODO: fetch weights from file 
-    return 1.
+	def deprel_sub_weight(self, edge1, edge2):
+	    return self.deprel_weights(edge1.deprel+"-"+edge2.deprel)
 
-def deprel_insdel_weight(edge):
-    #TODO: fetch weights from file 
-    return 1.
+	def deprel_insdel_weight(self, edge):
+		return self.deprel_weights(edge.deprel)
--- algorithm/graph_edit_dist.py
+++ algorithm/graph_edit_dist.py
@@ -7,31 +7,31 @@ import sys
 
 class GraphEditDistance(AbstractGraphEditDistance):
 
+    def __init__(self, g1, g2, pos_weights):
+        AbstractGraphEditDistance.__init__(g1, g2)
+        self.pos_weights = pos_weights
+
     def substitute_cost(self, node1, node2):
         return self.relabel_cost(node1, node2) + edge_diff(node1, node2)
 
     def delete_cost(self, i, j):
         if i == j:
-            return pos_insdel_weight(self.g1.node[i])
+            return self.pos_insdel_weight(self.g1.node[i])
         return sys.maxint
 
     def insert_cost(self, i, j):
         if i == j:
-            return pos_insdel_weight(self.g2.node[j])
+            return self.pos_insdel_weight(self.g2.node[j])
         else:
             return sys.maxint
 
+    def pos_sub_weight(self, node1, node2):
+        return self.pos_weights[node1.pos+"-"+node2.pos]
+
+    def pos_insdel_weight(self, node):
+        return self.pos_weights(node.pos)
+
 
 def edge_diff(node1, node2):
-    eed = EdgeEditDistance(node1.edges, node2.edges)
-    return eed.normalized_distance()
-
-
-def pos_sub_weight(node1, node2):
-    #TODO: fetch weights from file
-    return 1.
-
-
-def pos_insdel_weight(node):
-    #TODO: fetch weights from file
-    return 1.
+    edit_edit_dist = EdgeEditDistance(node1.edges, node2.edges)
+    return edit_edit_dist.normalized_distance()
