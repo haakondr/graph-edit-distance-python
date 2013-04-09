95bf0aba74f975221c9e10469e2619287841fa11
Håkon Drolsum Røkenes <hadr@steria.no>
2013-04-09 22:04:48 +0200

    cleanup

--- utils/misc.py
+++ utils/misc.py
@@ -1,11 +1,4 @@
-def chunks(l, n):
-    """
-    Yield n successive chunks from l.
-    """
-    newn = int(len(l) / n)
-    for i in xrange(0, n-1):
-        yield l[i*newn:i*newn+newn]
-    yield l[n*newn-newn:]
+# -*- coding: UTF-8 -*-
 
 
 def list_diff(list1, list2):
